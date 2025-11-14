83a12dabc609ed172cf43542d0d0635552f60481
Sir-Promise <officialprominent@gmail.com>
2025-11-14 04:24:42 +0100

    Add files via upload

---
+++ ErrorCode.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+public class TaxCalculator{
+   public static void main(String[] args){
+   Scanner scanner = new Scanner(System.in);
+   double taxRate1 = 0.15;
+   double taxrate2 = 0.20;
+   double incomeCeiling = 30000;
+
+   for (int i = 1; i <= 3; i++) {
+
+   System.out.print("Enter citizen " + i + "s name: ");
+   String name = scanner.nextLine();
+
+   System.out.print("Enter "+ name + "s income: $");
+   double income = scanner.nextDouble();
+   double tax;   
+   if (income <= incomeCeiling); {
+   tax = (incomeCeiling * taxRate1 ) + ((income - incomeCeiling) * taxRate2);
+   }
+   System.out.printf("%s's total tax is: $%,.2f%n%n", name, tax);
+
+   }
+
+  }
+}
---
+++ Table.java
@@ -0,0 +1,11 @@
+public class Table{
+   public static void main(String[] args) {
+
+    System.out.printf("%s\t%s\t%s\t%s%n", "N","N2,"N3", "N4")
+
+for (int N =1; N <= 5; N++) {
+
+    System.out.printf("%s\t%s\t%s\t%s%n", "N", "N²", "N³", "N⁴");
+    for (int N = 1; N <= 5; N++) {
+
+    System.out.printf("%d\t%d\t%d\t%d%n", N, N*N, N*N*N, N*N*N*N);
