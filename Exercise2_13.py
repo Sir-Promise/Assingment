50382d36b1df8c0af2f83f38bbabf4c3642fe6a3
Sir-Promise <officialprominent@gmail.com>
2025-11-14 01:00:04 +0100

    Add files via upload

---
+++ Exercise2_13.py
@@ -0,0 +1,17 @@
+number = 3 ** 300
+print(number)
+
+number = 3 ** 1000
+print(number)
+
+number = 3 ** 2000
+print (number)
+
+number = 100 ** 1000
+print (number)
+
+number = 500 ** 7000
+print (number)
+
+number = 1600 ** 600
+print (number)
